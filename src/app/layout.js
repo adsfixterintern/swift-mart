91872ba22b70f39f63d46cefddda6dff9ffba9e0
HFsa-RaShid <hafsa20cse028@gmail.com>
2026-02-18 14:27:34 +0600

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,14 +31,15 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Toaster position="top-center" reverseOrder={false} />
-<ThemeProvider><CartProvider>
-  <ReactQueryProvider>
-    <Navbar />
-      {children}
-    <Footer />
-  </ReactQueryProvider>
-</CartProvider></ThemeProvider>
-
+        <ThemeProvider>
+          <CartProvider>
+            <ReactQueryProvider>
+              <Navbar />
+              {children}
+              <Footer />
+            </ReactQueryProvider>
+          </CartProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
